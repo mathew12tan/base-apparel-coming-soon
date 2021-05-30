80f4c76abfaacf17d1f1840f768ea10585ae6ef0
mathew tan <mathew12tan@gmail.com>
2021-05-30 09:37:50 +0800

    second commit

--- app.js
+++ app.js
@@ -2,24 +2,23 @@ const emailInput = document.querySelector("#email");
 const emailPattern =
   /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-const emailDiv = document.querySelector(".email-div");
-const errorImg = document.querySelector(".error-img");
-const errorMsg = document.querySelector(".error-msg");
+const form = document.querySelector('form');
 const submitBtn = document.querySelector(".submit-btn");
+
+emailInput.addEventListener("click", function () {
+  form.classList.remove('error');
+});
+
+emailInput.addEventListener("keypress", function () {
+  form.classList.remove('error');
+});
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
   if (emailPattern.test(emailInput.value)) {
     alert("success");
-    emailDiv.classList.remove("email-div-error");
-    errorImg.classList.remove("error-img-show");
-    errorMsg.classList.remove("error-msg-show");
-    submitBtn.classList.remove("submit-btn-error");
     emailInput.value = "";
   } else {
-    emailDiv.classList.add("email-div-error");
-    errorImg.classList.add("error-img-show");
-    errorMsg.classList.add("error-msg-show");
-    submitBtn.classList.add("submit-btn-error");
+    form.classList.add('error');
   }
 });
